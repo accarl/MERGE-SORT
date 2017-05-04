04a02a5a0b75048c9af0a6e628ced461f4a82864
chadlr <delosreyescharissa@gmail.com>
2017-05-04 22:12:36 +0800

    Added quicksort
    quick sort is working fine, except if data input has repeated value

--- MergeSort_0.cpp
+++ MergeSort_0.cpp
@@ -61,6 +61,51 @@ void MergeSort(int theArray[], int length, int first, int last, int* divideCount
 		Merge(theArray, length, first, mid, last, compareCount, movesCount);
 	} // end if
 }
+int Partition(int T[], int length, int first, int last)
+{
+     int pivot, temp;
+     int loop, cutPoint, bottom, top;
+     //cutPoint = 0;
+     pivot = T[first];
+     bottom = first;
+     top = last;
+     loop = 1;
+     while (loop){
+           while (T[top]>pivot)
+           { top--;}
+           //cout<<"top"<<T[top]<<endl;
+           while (T[bottom]<pivot)
+           { bottom++;}
+           //cout<<"bottom"<<T[bottom]<<endl;
+     if (bottom < top){
+           temp = T[bottom];
+           T[bottom]=T[top];
+           T[top]=temp;
+           }
+     else {
+          loop=0;
+          cutPoint=top;
+          
+          }
+     }
+     //cout<<"cutpoint"<<cutPoint<<endl;
+     return cutPoint;
+} //end partition()
+
+void QuickSort (int T[], int length, int first, int last)
+{
+     int cut;
+     if (first < last){
+          //cout<<"check0"<<endl;
+          cut = Partition(T,length,first,last);
+          //cout<<"check1"<<endl;
+          QuickSort(T, length, first, cut);
+          //cout<<"check2"<<endl;
+          QuickSort(T, length, cut+1, last);
+          //cout<<"check3"<<endl;
+          
+     }
+}
 
 void GenerateRandomNumbers(int numbers[], int length){
 	srand(time(NULL));
@@ -70,7 +115,7 @@ void GenerateRandomNumbers(int numbers[], int length){
 }
 
 void PrintData(int data[], int length){
-	for(int i = 0; i < 100; i++){
+	for(int i = 0; i < length; i++){
 		
 		if(i%10 == 0){
 			cout<<endl;
@@ -84,25 +129,33 @@ void PrintData(int data[], int length){
 }
 
 void OneHundredData(){
-	int length = 100;
-	int data[length];
+	int length = 11;
+	//int data[length];
+	int data[] = {749, 437, 730, 935, 987, 826, 121, 414, 979, 238, 5};
 	int tempdata[length];
 	int divCount, comCount, movCount;
 	divCount = 0;
 	comCount = 0;
 	movCount = 0;
+	//cutPoint = 0;
 	
 	cout<<"Generating Random Numbers....."<<endl;
-	GenerateRandomNumbers(data, length);
+	//GenerateRandomNumbers(data, length);
 	CopyArray(data, tempdata, length);
 	PrintData(data, length);	
-	cout<<endl<<"Sorting..."<<endl<<endl;
+	cout<<endl<<"Sorting...Merge"<<endl<<endl;
 	MergeSort(data, length, 0, length - 1, &divCount, &comCount, &movCount);
 	PrintData(data, length);
 	
 	cout<<endl<<endl<<"Division count: "<<divCount<<endl;
 	cout<<"Compare count: "<<comCount<<endl;
 	cout<<"Moves count: "<<movCount<<endl;
+	
+	//CopyArray(data, tempdata, length);
+	PrintData(tempdata, length);
+	cout<<endl<<"Sorting...Quick"<<endl<<endl;
+    QuickSort(tempdata, length, 0, length - 1);
+	PrintData(tempdata, length);
 }
 
 void FiveHundredData(){
